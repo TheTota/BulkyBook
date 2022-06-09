6581e81b1557acbabaf0cb1ec8d9f8e65863a991
Thomas Cianfarani <thomas.ci83@gmail.com>
2022-06-09 10:49:38 +0200

    update dataaccess for cover types

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType obj); // Update is specific to each model, so it is part of this interface and not IRepository
+    }
+}
