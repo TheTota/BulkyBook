277428bed33871ef97926886c0264fc3920a0f97
Thomas Cianfarani <thomas.ci83@gmail.com>
2022-06-09 11:40:49 +0200

    add product in dataaccess

---
+++ BulkyBook.DataAccess/Repository/IRepository/IProductRepositiory.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj); // Update is specific to each model, so it is part of this interface and not IRepository
+    }
+}
--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -12,7 +12,7 @@ namespace BulkyBook.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         public string Description { get; set; }
 
