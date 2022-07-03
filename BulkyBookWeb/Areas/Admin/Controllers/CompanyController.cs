46b522c7116df8776c137a2e426b0ed141a28cb1
Thomas Cianfarani <thomas.ci83@gmail.com>
2022-07-03 15:01:14 +0200

    add company to db and CRUD operations

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,92 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Company> objects = _unitOfWork.Company.GetAll();
+            return View(objects);
+        }
+
+        // GET 
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new();
+
+            if (id == null || id == 0)
+            {
+                // create product
+                return View(company);
+            }
+            else
+            {
+                // update product
+                company = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
+                return View(company);
+            }
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    TempData["success"] = "Company updated successfully";
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        #region API CALLS
+
+        // can preview the Json with the url .../admin/product/getall
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
